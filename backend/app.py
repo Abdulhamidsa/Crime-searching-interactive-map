f2add21859175bb274f85bb1045f7265a8c06ff0
Aboood <abdu3793@stud.kea.dk>
2024-05-02 12:33:50 +0200

    "..."

--- backend/app.py
+++ backend/app.py
@@ -1,15 +1,33 @@
+from bottle import Bottle, request, response, run
 import requests
+import json
+
+app = Bottle()
+
+# Function to check token against PythonAnywhere
+def check_token(username, token):
+    response = requests.get(
+        'https://www.pythonanywhere.com/api/v0/user/{username}/cpu/'.format(
+            username=username
+        ),
+        headers={'Authorization': 'Token {token}'.format(token=token)}
+    )
+    return response.status_code
+
 username = 'Abdulhamidsa'
 token = '2d49d619c5c231b2c7743d9ffaf3201980bf711a'
 
-response = requests.get(
-    'https://www.pythonanywhere.com/api/v0/user/{username}/cpu/'.format(
-        username=username
-    ),
-    headers={'Authorization': 'Token {token}'.format(token=token)}
-)
-if response.status_code == 200:
-    print('CPU quota info:')
-    print(response.content)
-else:
-    print('Got unexpected status code {}: {!r}'.format(response.status_code, response.content))
+@app.get('/crimes')
+def get_crimes():
+    if check_token(username, token) == 200:
+        crimes_response = requests.get('https://abdulhamidsa.pythonanywhere.com/crimes')
+        if crimes_response.status_code == 200:
+            return json.dumps(crimes_response.json())
+        else:
+            response.status = crimes_response.status_code
+            return json.dumps({"error": "Failed to fetch crime data."})
+    else:
+        return json.dumps({"error": "Token is invalid."})
+
+if __name__ == '__main__':
+    run(app, host='localhost', port=8080, debug=True, reload=True)
